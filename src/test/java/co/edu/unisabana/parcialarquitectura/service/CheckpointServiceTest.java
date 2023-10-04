c6349e5a27b02d8a3c21bcc05dfa4f5c8ae4dd48
jeanvaes <jeanvaes@unisabana.edu.co>
2023-10-04 18:34:54 -0500

    verificar ci

--- src/test/java/co/edu/unisabana/parcialarquitectura/service/CheckpointServiceTest.java
+++ src/test/java/co/edu/unisabana/parcialarquitectura/service/CheckpointServiceTest.java
@@ -1,10 +1,21 @@
 package co.edu.unisabana.parcialarquitectura.service;
 
+import co.edu.unisabana.parcialarquitectura.repository.jpa.CheckpointRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class CheckpointServiceTest {
 
-    void Dado_checkinDTO_Cuando_checkin_Entonces_(){}
+    @InjectMocks
+    CheckpointService service;
+
+    @Mock
+    CheckpointRepository checkpointRepository;
+
+    void Dado_checkinDTO_Cuando_checkin_Entonces_(){
+
+    }
 }
