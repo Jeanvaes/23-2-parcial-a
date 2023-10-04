83d45e3f205c1448c7a0e3dc8d6a83e6664e1bb4
jeanvaes <jeanvaes@unisabana.edu.co>
2023-10-04 18:17:04 -0500

    agregar test service

---
+++ src/test/java/co/edu/unisabana/parcialarquitectura/service/CheckpointServiceTest.java
@@ -0,0 +1,8 @@
+package co.edu.unisabana.parcialarquitectura.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CheckpointServiceTest {
+}
